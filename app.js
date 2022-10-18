8d213fce534bf23399bcd692d766f17612b47387
Ghasb001 <hasbun.guillermo@gmail.com>
2022-10-17 21:40:48 -0400

    Complete Bare Minimum Requirements

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item, i) =>
+      <GroceryListItem key={i} item={item} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+  	super(props);
+    //default hover state
+  	this.state = {
+  		hover: false
+  	};
+  }
+  //make a hover function for the grocery list
+  onListHover() {
+  	this.setState({
+  		hover: !this.state.hover
+  	});
+  }
+  //actually render the thing
+  render() {
+    var style = {
+    	fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+  	return (
+      <li style={style} onMouseOver={this.onListHover.bind(this)}>{this.props.item}</li>
+  	);
+  }
+}
+
+ReactDOM.render(<GroceryList items={['beef', 'rice']}/>, document.getElementById('app'));
